1ff8ce77b3be125c8d601aaeae9b2b1d664b8015
Jose Garcia <josegarcia19809@gmail.com>
2025-01-11 08:51:02 -0600

    Inyección de dependencias(Factura): Factura y relaciones entre sus componentes

--- src/main/java/com/inyeccion_dependencias/factura/models/Product.java
+++ src/main/java/com/inyeccion_dependencias/factura/models/Product.java
@@ -1,4 +1,22 @@
 package com.inyeccion_dependencias.factura.models;
 
 public class Product {
+    private String name;
+    private Integer price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
 }
